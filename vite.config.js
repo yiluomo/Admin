9f8be87f5bf0ffd4dc8df7039b2170b073c07503
yiluomo <147462910+yiluomo@users.noreply.github.com>
2024-11-24 15:47:16 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/Admin/'
   plugins:
    [vue(),
     AutoImport({
